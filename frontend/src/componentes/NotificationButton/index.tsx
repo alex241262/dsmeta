2883a8b2dacac4bb9c7f25578eca024b8aadafcd
alex241262 <alexandreferreira168@gmail.com>
2022-09-11 14:24:23 -0400

    Toast

--- frontend/src/componentes/NotificationButton/index.tsx
+++ frontend/src/componentes/NotificationButton/index.tsx
@@ -1,9 +1,22 @@
 import icon from '../../assets/img/notification-icon.svg'
+import axios from "axios";
+import { BASE_URL } from '../../utils/request';
 import './styles.css'
+import { Toast } from 'react-toastify/dist/components';
+import { toast } from 'react-toastify';
 
-function NotificationButton() {
+type Props= {
+    saleId:number;
+}
+function handleClick(id : number){
+    axios(`${BASE_URL}/sales/${id}/notification`).then(
+        response => {toast.info(" SMS ENVIADO COM SUCESSO");}
+    );
+}
+
+function NotificationButton({saleId}: Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
